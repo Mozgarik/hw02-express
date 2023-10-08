0ee77c2a2b97fe14262ebdf950f67d47c0c63f03
Mozgarik <mozgarik1613@gmail.com>
2023-10-08 19:21:06 +0300

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,9 @@ import express from 'express'
 import contactService from '../../models/contacts.js'
 import HttpError from '../../helpers/HttpError.js'
 import Joi from 'joi'
+import Contact from '../../models/contact.js'
+import { isValidObjectId } from 'mongoose'
+
 
 const contactAddSchema = Joi.object({
  name: Joi.string()
@@ -26,23 +29,39 @@ const contactAddSchema = Joi.object({
   })
 })
 
+const contactFavoriteUpdate = Joi.object({
+  favorite: Joi.boolean().required()
+})
+
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
   try {
+<<<<<<< Updated upstream
     const result = await contactService.listContacts()
+=======
+    const result = await Contact.find()
+    console.log(result)
+>>>>>>> Stashed changes
   res.json(result)
   } catch (error) {
     res.status(500).json({
       message: 'Server error'
     })
   }
-  
 })
 
 router.get('/:contactId', async (req, res, next) => {
+<<<<<<< Updated upstream
+=======
+  const id = req.params.contactId
+   if (!isValidObjectId(id)) {
+      return next(HttpError(404, `${id} not valid id`))
+  }
+>>>>>>> Stashed changes
   try {
-    const result = await contactService.getContactById(req.params.contactId)
+    const result = await Contact.findById(id)
+   
     if(!result){
       throw HttpError(404, `contact with id: ${req.params.contactId} not found`)
     }
@@ -55,27 +74,26 @@ router.get('/:contactId', async (req, res, next) => {
   }
 })
 
+
 router.post('/', async (req, res, next) => {
-  try {
-    const validateContact = contactAddSchema.validate(req.body)
-    if(!validateContact.error) {
-      const result = await contactService.addContact(req.body)
+  try {  
+      const result = await Contact.create(req.body)
       res.status(201).json(result)
-    } else {
-        console.log(validateContact.error)
-         res.status(400).json({
-          message: validateContact.error.message
-         })
     }
-} catch (error) {
-  next(error)
+ catch (error) {
+  res.status(error.status).json({
+    message: error.message
+  })
 }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
+  const contactId = req.params.contactId
+  if (!isValidObjectId(contactId)) {
+    return next(HttpError(404, `${id} not valid id`))
+}
 try {
-  const {contactId} = req.params
-  const result = await contactService.removeContact(contactId)
+  const result = await Contact.findByIdAndDelete(contactId)
   if (!result) {
     res.status(404).json({
       message: `not found`
@@ -92,9 +110,14 @@ try {
 })
 
 router.put('/:contactId', async (req, res, next) => {
+  const id = req.params.contactId
+  if (!isValidObjectId(id)) {
+    return next(HttpError(404, `${id} not valid id`))
+}
   try {
     const validateContact = contactAddSchema.validate(req.body)
     if(!validateContact.error) {
+<<<<<<< Updated upstream
       const id = req.params.contactId
       const result = await contactService.updateContactById(id, req.body)
       if(result === null) {
@@ -104,6 +127,10 @@ router.put('/:contactId', async (req, res, next) => {
       }else {
         res.status(200).json(result)
       }
+=======
+      const result = await Contact.findByIdAndUpdate(id, req.body, {new: true, runValidators: true})
+      res.status(200).json(result)
+>>>>>>> Stashed changes
     } else {
          res.status(400).json({
           message: validateContact.error.message
@@ -113,5 +140,36 @@ router.put('/:contactId', async (req, res, next) => {
   next(error)
 }
 })
+
+
+
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  const id = req.params.contactId
+  if (!isValidObjectId(id)) {
+    return next(HttpError(404, `${id} not valid id`))
+}
+  try {
+    if (Object.keys(req.body).length !== 0) {
+      const validateContact = contactFavoriteUpdate.validate(req.body)
+    if(!validateContact.error) {
+      const result = await Contact.findByIdAndUpdate(id, req.body, {new: true})
+      res.status(200).json(result)
+    } else {
+         res.status(400).json({
+          message: contactFavoriteUpdate.error.message
+         })
+    }
+    }else {
+      res.status(400).json({
+        message: 'missing fields'
+      })
+    }
+} catch (error) {
+  next(error)
+}
+})
+
+
+
 
 export default router
